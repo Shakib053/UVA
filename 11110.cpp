0392a895aefa6a460583a9b522e464815658f955
Shakib053 <1505053@student.ruet.ac.bd>
2021-12-29 00:09:58 +0600

    Equidivisions

--- 11110.cpp
+++ 11110.cpp
@@ -4,41 +4,86 @@ int grid[101][101];
 int n;
 int arr[202];
 int cnt;
+int cntarr[101];
+int check;
+
 void initgrid()
 {
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
-            grid[i][j] = 0;
+            grid[i][j] = n;
+    }
+}
+void dfs(int x, int y, int val)
+{
+    grid[x][y] = 0;
+    cnt++;
+    if (x + 1 < n && grid[x + 1][y] == val)
+        dfs(x + 1, y, val);
+    if (x - 1 >= 0 && grid[x - 1][y] == val)
+        dfs(x - 1, y, val);
+    if (y + 1 < n && grid[x][y + 1] == val)
+        dfs(x, y + 1, val);
+    if (y - 1 >= 0 && grid[x][y - 1] == val)
+        dfs(x, y - 1, val);
+}
+void solve()
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cnt = 0;
+            if (grid[i][j] != 0)
+            {
+                dfs(i, j, grid[i][j]);
+                if (cnt != n)
+                {
+                    check = 1;
+                    break;
+                }
+            }
+        }
+        if (check == 1)
+            break;
     }
 }
 int main()
 {
-    while (1)
+    while (1 == scanf("%d", &n) && n)
     {
-        cin >> n;
-        if (n == 0)
-            break;
-        cnt = 1;
-        for (int i = 0; i < n - 1; i++)
+        initgrid();
+        int k, p, q;
+        char ch;
+        for (int i = 1; i < n; i++)
         {
-            for (int j = 0; j < 2 * n; j++)
+            while (1)
             {
-                cin >> arr[j];
+                scanf("%d %d%c", &p, &q, &ch);
+                grid[p - 1][q - 1] = i;
+                if (ch != ' ')
+                    break;
             }
-            for (int k = 0; k < 2 * n; k = k + 2)
-            {
-                grid[arr[k] - 1][arr[k + 1] - 1] = cnt;
-            }
-            cnt++;
         }
-        cout << "Grid Layout" << endl;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-                cout << grid[i][j] << " ";
-            cout << endl;
-        }
+        check = 0;
+        solve();
+        // cout << "Grid Layout" << endl;
+        // for (int i = 0; i < n; i++)
+        // {
+        // 	for (int j = 0; j < n; j++)
+        // 		cout << grid[i][j] << " ";
+        // 	cout << endl;
+        // }
+        // cout << "Cnt Arry" << endl;
+        // for (int i = 0; i < n; i++)
+        // 	cout << cntarr[i] << " ";
+        // cout << endl;
+        if (check == 0)
+            cout << "good" << endl;
+        else
+            cout << "wrong" << endl;
     }
 
     return 0;
