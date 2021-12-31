fe3d82371e207e07b6aec54cb8f15743201fbe68
Shakib053 <1505053@student.ruet.ac.bd>
2022-01-01 01:27:51 +0600

    Bicoloring

---
+++ 10004.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+int n, edges;
+int grid[1000][1000];
+int visited[1000];
+int queue[1000];
+int color[1000];
+int front, rear, start;
+int bipartite;
+void print()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << grid[i][j] << " ";
+        cout << endl;
+    }
+}
+void initcase()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            grid[i][j] = 0;
+    }
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+    for (int i = 0; i < n; i++)
+        color[i] = 0;
+    bipartite = 1;
+}
+void enqueue(int val)
+{
+    queue[rear] = val;
+    rear++;
+}
+int dequeue()
+{
+    int element = queue[front];
+    front++;
+    return element;
+}
+
+void bfs(int x)
+{
+    enqueue(x);
+    visited[x] = 1;
+
+    while (front < rear)
+    {
+        int value = dequeue();
+        // cout << value << " ";
+        // color[value] = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (grid[value][i] == 1)
+            {
+                if (visited[i] == 0)
+                {
+                    visited[i] = 1;
+                    int v = grid[value][i];
+                    // color[i] = 1 - color[i];
+
+                    color[i] = 1 - color[value];
+                    enqueue(i);
+                }
+                else if (visited[i] == 1)
+                {
+                    if (color[i] == color[value])
+                    {
+                        bipartite = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // cout << endl;
+}
+int main()
+{
+    while (1)
+    {
+        cin >> n;
+        front = 0, rear = 0;
+
+        if (n == 0)
+            break;
+        cin >> edges;
+        initcase();
+        for (int i = 0; i < edges; i++)
+        {
+            int x, y;
+            cin >> x >> y;
+            grid[x][y] = 1;
+            grid[y][x] = 1;
+        }
+        // print();
+        bfs(0);
+        if (bipartite == 0)
+            cout << "NOT BICOLORABLE.\n";
+        else
+            cout << "BICOLORABLE.\n";
+    }
+
+    return 0;
+}
